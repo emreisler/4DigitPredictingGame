a053f1e3547b87a1ef0a4a5ecec33ffacec1a139
emreisler <emre.isler.9@gmail.com>
2021-03-27 20:33:58 +0300

    Updated

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from AiPlayer import Ai
 def main():
 
 
-    for i in range(90):
-        hamle = 0
-        aiPlayer = Ai()
-        while not aiPlayer.gameOver:
-            hamle += 1
-            prediction = aiPlayer.predict()
-            print("PREDICTION : ",prediction)
-            hint = aiPlayer.giveHint(prediction)
-            print(hint)
-            aiPlayer.evaluate(hint,prediction)
-            print(aiPlayer.tumOlasiliklar)
-            aiPlayer.normalize()
-            #aiPlayer.evaluate(prediction)
-        print("HAMLE SAYISI : ", hamle)
-        print("GAME OVER")
+    #for i in range(90):
+    hamle = 0
+    aiPlayer = Ai()
+    while not aiPlayer.gameOver:
+        hamle += 1
+        prediction = aiPlayer.predict()
+        print("PREDICTION : ",prediction)
+        hint = aiPlayer.giveHint(prediction)
+        print(hint)
+        aiPlayer.evaluate(hint,prediction)
+        print(aiPlayer.tumOlasiliklar)
+        aiPlayer.normalize()
+        #aiPlayer.evaluate(prediction)
+    print("HAMLE SAYISI : ", hamle)
+    print("GAME OVER")
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
