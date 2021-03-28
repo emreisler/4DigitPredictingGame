84c1151e654f638a284adfefdbba13f442a76b36
emreisler <emre.isler.9@gmail.com>
2021-03-28 21:08:44 +0300

    Code refactor

--- AiPlayer.py
+++ AiPlayer.py
@@ -2,27 +2,36 @@
 class Ai:
 
     def __init__(self):
-
-        self.allPossibilities = {}
-        self.numbers = [0,1,2,3,4,5,6,7,8,9]
-        self.numberRange = len(self.numbers)
+        '''
+        İnitilize required fields for Ai objects.
+        '''
+        self.allPossibilities = {} # initilize a dict to keep all possible moves
+        self.numbers = [0,1,2,3,4,5,6,7,8,9] # initilize numbers
+        self.numberRange = len(self.numbers) # calculate length of numbers 
         self.createAllPossibilities(self.numbers)
-        self.gameOver = False
-        self.targetNumber = None
+        self.gameOver = False # initilize gameOver as false
+        self.targetNumber = None # initilize a targetNumber field
 
+    
     def createAllPossibilities(self,numbers):
+        '''
+        Creates all possibilities in dict and assign their initial rankings
+        '''
         for i in range(self.numberRange):
             for j in range(self.numberRange):
                 for k in range(self.numberRange):
                     for l in range(self.numberRange):
                         if(i != j) and (i != k) and (i != l) and (j != k) and (j != l) and (k != l):
                             self.allPossibilities[(i,j,k,l)] = 0
-
+                            
+        #assign initial rankings of dict elements
         for combination in self.allPossibilities:
             self.allPossibilities[combination] = 1 / len(self.allPossibilities)
         
     def predict(self):
-        
+        '''
+        Makes prediction by choosing the highest rating one from all possibilities dict.
+        '''
         highestScore = 0
         highest = ()
         for combination in self.allPossibilities:
@@ -33,19 +42,30 @@ class Ai:
 
     def evaluate(self,point,prediction):
         
+        '''
+        Evaluates the prediction per given hint
+        '''
+        
+        # GameOver condition
         if point[0] == 4:
             print("*************************")
             print("YOUR NUMBER IS FOUND: ",prediction[0],prediction[1],prediction[2],prediction[3])
             print("*************************")
             self.gameOver = True
-            
+        
+        #If game is not over than eleminate the current prediction    
         self.allPossibilities[prediction] = 0
+        
+        #Loop through all possible moves and if their hint for current prediction does not match with current hint, eleminate them.
         for combination in self.allPossibilities:
             if self.giveHint(combination,prediction) != point:
                 self.allPossibilities[combination] = 0
         
         
     def giveHint(self,prediction,target):
+        '''
+        Calculates and returns hint
+        '''
         pointsA = 0
         pointsB = 0
         for i in range(4):
@@ -56,4 +76,4 @@ class Ai:
                     pointsB += 1
                     
         return pointsA,pointsB
-        
+    
--- main.py
+++ main.py
@@ -1,23 +1,23 @@
 from AiPlayer import Ai
 
 def main():
-
-    move = 0
-    aiPlayer = Ai()
-    human_number = input("Enter your number : ")
-    aiPlayer.targetNumber = (int(human_number[0]),int(human_number[1]),int(human_number[2]),int(human_number[3]))
+    '''
+    Error states are not implemented just because.
+    '''
+    move = 0 # initilize move counter
+    aiPlayer = Ai() # create aiPlayer object
+    human_number = input("Enter your number : ") # request number
+    aiPlayer.targetNumber = (int(human_number[0]),int(human_number[1]),int(human_number[2]),int(human_number[3])) #take human number as tuple 
     
     while not aiPlayer.gameOver:
         move += 1
-        prediction = aiPlayer.predict()
+        prediction = aiPlayer.predict() # make prediction
         print("PREDICTION",move," : ",prediction[0],prediction[1],prediction[2],prediction[3])
-        hint = aiPlayer.giveHint(prediction,aiPlayer.targetNumber)
+        hint = aiPlayer.giveHint(prediction,aiPlayer.targetNumber) # calculate and give hint
         print("HINT : ", hint[0],"a ",hint[1],"b")
-        aiPlayer.evaluate(hint,prediction)
+        aiPlayer.evaluate(hint,prediction) # evaluate the prediction per hint
         
-       
-        
-    print("TOTAL MOVES : ", move)
+    print("TOTAL MOVES : ", move) # print total moves
     print("GAME OVER")
     
 
