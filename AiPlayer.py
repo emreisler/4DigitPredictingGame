16bc346e1dc2eb82ad9849f27c3da89e0c3a39b6
emreisler <emre.isler.9@gmail.com>
2021-03-28 20:39:18 +0300

    Code refactor

--- AiPlayer.py
+++ AiPlayer.py
@@ -1,76 +1,50 @@
-import random
 
 class Ai:
 
     def __init__(self):
 
-        self.tumOlasiliklar = {}
-        self.rakamlar = [0,1,2,3,4,5,6,7,8,9]
-        self.tumOlasiliklarOlustur(self.rakamlar)
+        self.allPossibilities = {}
+        self.numbers = [0,1,2,3,4,5,6,7,8,9]
+        self.numberRange = len(self.numbers)
+        self.createAllPossibilities(self.numbers)
         self.gameOver = False
-        self.targetNumber = self.randomNumber(self.rakamlar)
-        self.moveCount = 0
-        self.alpha = 0.8
-        self.initialRandomTrial = 7
-        self.predictionHistory = []
-        
-        
+        self.targetNumber = None
 
-    def tumOlasiliklarOlustur(self,rakamlar):
-        for i in range(len(rakamlar)):
-            for j in range(len(rakamlar)):
-                for k in range(len(rakamlar)):
-                    for l in range(len(rakamlar)):
+    def createAllPossibilities(self,numbers):
+        for i in range(self.numberRange):
+            for j in range(self.numberRange):
+                for k in range(self.numberRange):
+                    for l in range(self.numberRange):
                         if(i != j) and (i != k) and (i != l) and (j != k) and (j != l) and (k != l):
-                            self.tumOlasiliklar[(i,j,k,l)] = 0
+                            self.allPossibilities[(i,j,k,l)] = 0
 
-        for combination in self.tumOlasiliklar:
-            self.tumOlasiliklar[combination] = 1 / len(self.tumOlasiliklar)
-        
+        for combination in self.allPossibilities:
+            self.allPossibilities[combination] = 1 / len(self.allPossibilities)
         
     def predict(self):
         
         highestScore = 0
         highest = ()
-        for combination in self.tumOlasiliklar:
-            if self.tumOlasiliklar[combination] > highestScore:
-                highestScore = self.tumOlasiliklar[combination]
+        for combination in self.allPossibilities:
+            if self.allPossibilities[combination] > highestScore:
+                highestScore = self.allPossibilities[combination]
                 highest = combination
-        self.moveCount += 1
         return highest
 
-    def evaluate(self,puan,prediction):
+    def evaluate(self,point,prediction):
         
-        if puan[0] == 4:
+        if point[0] == 4:
             print("*************************")
-            print("YOUR NUMBER : ",prediction[0],prediction[1],prediction[2],prediction[3])
+            print("YOUR NUMBER IS FOUND: ",prediction[0],prediction[1],prediction[2],prediction[3])
             print("*************************")
-            print("GAME OVER")
             self.gameOver = True
             
-        self.tumOlasiliklar[prediction] = 0
-        for combination in self.tumOlasiliklar:
-            if self.giveHint(combination,prediction) != puan:
-                self.tumOlasiliklar[combination] = 0
+        self.allPossibilities[prediction] = 0
+        for combination in self.allPossibilities:
+            if self.giveHint(combination,prediction) != point:
+                self.allPossibilities[combination] = 0
         
-            
-    def normalize(self):
-        total = 0
-        for combination in self.tumOlasiliklar:
-            total += self.tumOlasiliklar[combination]
-
-        for combination in self.tumOlasiliklar:
-            try:
-                
-                self.tumOlasiliklar[combination]  = self.tumOlasiliklar[combination] / total
-            except :
-                pass
-        #print(self.tumOlasiliklar)
-
-
-    def print(self):
-        print(self.tumOlasiliklar)
-
+        
     def giveHint(self,prediction,target):
         pointsA = 0
         pointsB = 0
@@ -82,39 +56,4 @@ class Ai:
                     pointsB += 1
                     
         return pointsA,pointsB
-      
-
-    def randomNumber(self,rakamlar):
-        
-        copyRakamlar = rakamlar.copy()
-        number1 = copyRakamlar.pop(random.randint(0,9))
-        number2 = copyRakamlar.pop(random.randint(0,8))
-        number3 = copyRakamlar.pop(random.randint(0,7))
-        number4 = copyRakamlar.pop(random.randint(0,6))
-        
-        
-        return number1,number2,number3,number4
-    
-    def zeroPossibilitesList(self):
-        zeroPoss = 0
-        for combination in self.tumOlasiliklar:
-            if self.tumOlasiliklar[combination] == 0:
-                zeroPoss += 1
-        
-        
-    
-    def checkHistory(self,prediction):
-        if len(self.predictionHistory) > 0:
-            for combination in self.predictionHistory:
-                sameNumberPredicted = 0
-                for i in range(4):
-                    if prediction[i] in combination:
-                        sameNumberPredicted += 1
-                if sameNumberPredicted > 2:
-                    return False
-                else:
-                    return True
-        else:
-            return True
-        
         
--- main.py
+++ main.py
@@ -1,36 +1,25 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from AiPlayer import Ai
 
 def main():
 
-    toplamHamleSayisi = 0
-    #totalGAmes = 50
-    #for i in range(totalGAmes):
-    hamle = 0
+    move = 0
     aiPlayer = Ai()
     human_number = input("Enter your number : ")
     aiPlayer.targetNumber = (int(human_number[0]),int(human_number[1]),int(human_number[2]),int(human_number[3]))
+    
     while not aiPlayer.gameOver:
-        hamle += 1
+        move += 1
         prediction = aiPlayer.predict()
-        print("PREDICTION : ",prediction[0],prediction[1],prediction[2],prediction[3])
+        print("PREDICTION",move," : ",prediction[0],prediction[1],prediction[2],prediction[3])
         hint = aiPlayer.giveHint(prediction,aiPlayer.targetNumber)
         print("HINT : ", hint[0],"a ",hint[1],"b")
         aiPlayer.evaluate(hint,prediction)
-        #print(aiPlayer.tumOlasiliklar)
-        aiPlayer.normalize()
-        #aiPlayer.evaluate(prediction)
-        aiPlayer.zeroPossibilitesList()
-    print("TOTAL MOVES : ", hamle)
+        
+       
+        
+    print("TOTAL MOVES : ", move)
     print("GAME OVER")
-    toplamHamleSayisi += hamle
-    #print("ORTALAMA KAÇ HAMLEDE BULDU : ", toplamHamleSayisi / totalGAmes )
+    
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
